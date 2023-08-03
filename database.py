471eaeeac8262fcaca107d969496b5eb31a05c79
serg_myst <serg_myst@mail.ru>
2023-08-03 14:28:54 +0300

    html, new table

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from config import DATABASE
+from config import DATABASE, BASE_TYPE
 
 DATABASE_URL = DATABASE
 
-engine = create_engine(DATABASE_URL, echo=False, client_encoding = 'utf8')
+engine = create_engine(DATABASE_URL, echo=False)
+if BASE_TYPE == 'PGSQL':
+    engine = create_engine(DATABASE_URL, echo=False, client_encoding='utf8')
+
 session_maker = sessionmaker(engine, expire_on_commit=False)
 
 # Работа с SQLAlchemy
